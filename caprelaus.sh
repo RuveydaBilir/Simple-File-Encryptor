4daa2a65d5971f202f27af4a9ae6ebd99bdb79b6
ruveyda <126386786+RuveydaBilir@users.noreply.github.com>
2024-08-01 09:58:12 +0300

    Adding logic for lock function
    Function changes to prevent locking itself and already locked files.

--- caprelaus.sh
+++ caprelaus.sh
@@ -5,37 +5,41 @@ echo "                          operation: 1.1"
 echo ""
 
 help(){
-echo "Enter action and directory:"
-echo "-change : to change your password"
-echo "-lock : to lock files"
-echo "-unlock : to unlock files"
-exit 1
+        echo "Usage: ./caprelaus.sh {action} {target_directory}"
+        echo "       or ./caprelaus.sh change"
+        echo "-change : to change your password"
+        echo "-lock : to lock files"
+        echo "-unlock : to unlock files"
+        exit 1
 }
 
 ACTION=$1
 DIR=$2
 HASH="hash.txt"
+SCRIPT_NAME="$(basename "$0")"
 
 lock(){
-if [[ -d $DIR ]]
-then
-echo "Locking files in directory: $DIR"
-
-for FILE in "$DIR"/*; do
-        if [[ -f $FILE ]]; then
-        if [[ $FILE != $HASH ]]; then
-        echo Locking file: $FILE
-        openssl enc -aes-256-cbc -salt -pbkdf2 -in $FILE -out "$FILE".cap -pass pass:$PASS
-        if [[ $? -eq 0 ]]
+        if [[ -d $DIR ]]
         then
-            rm "$FILE"
-        else
-            echo "Failed to lock the file: $FILE"
+        echo "Locking files in directory: $DIR"
+        for FILE in "$DIR"/*; do
+                if [[ -f $FILE ]]; then
+                        FILE_NAME=$(basename "$FILE")
+                        if [[ $FILE_NAME != $HASH && $FILE_NAME != $SCRIPT_NAME && "$FILE" != *.cap ]]; then
+                        echo Locking file: $FILE
+                        openssl enc -aes-256-cbc -salt -pbkdf2 -in $FILE -out "$FILE".cap -pass pass:$PASS
+                                if [[ $? -eq 0 ]]
+                                then
+                                    rm "$FILE"
+                                else
+                                    echo "Failed to lock the file: $FILE"
+                                fi
+                        else
+                                echo "Skipping file: $FILE"
+                        fi
+                fi
+        done
         fi
-        fi
-        fi
-done
-fi
 }
 unlock(){
         if [[ -d $DIR ]]; then
@@ -80,7 +84,7 @@ start(){
                         change
                         ;;
                 *)
-                         usage
+                         help
                         ;;
                 esac
 
